fdfe023a60899f5f2501b13e30b0bb33a995a5a7
przemyslaw <przemyslaw@onegini.com>
2022-03-31 22:14:03 +0200

    base project structure

---
+++ src/main/java/pl/beusable/RoomManagerApplication.java
@@ -0,0 +1,21 @@
+package pl.beusable;
+
+import static pl.beusable.room.RoomType.PREMIUM;
+import static pl.beusable.room.RoomType.STANDARD;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.beusable.room.Room;
+
+import java.util.List;
+
+@SpringBootApplication
+public class RoomManagerApplication {
+
+
+  public static void main(String[] args) {
+
+
+
+  }
+
+}
---
+++ src/test/java/pl/beusable/RoomManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.beusable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
